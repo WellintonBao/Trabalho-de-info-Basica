9ecfc247d59de8976f5a66f01f4a9de4c7453bc1
Wellinton <wellintonbao@gmail.com>
2023-02-08 21:30:33 -0300

    adicionei o arquivo inicial de repositorio

---
+++ lamport.py
@@ -0,0 +1,82 @@
+def resumo():
+    mensagem = "Alan Mathison Turing  foi um matemático britânico, pioneiro da computação e considerado o pai da ciência computacional e da inteligência artificial."
+    return mensagem
+
+
+def doutorado():
+    mensagem = ""
+    return mensagem
+
+
+def contribuicoes():
+    mensagem = ""
+    return mensagem
+
+
+def artigos():
+    mensagem = ""
+    return mensagem
+
+
+def citacoes():
+    mensagem = ""
+    return mensagem
+
+
+def sair():
+    mensagem = "\nObrigado pela leitura!"
+    return mensagem
+
+
+def erro():
+    mensagem = "\nOpção inválida!"
+    return mensagem
+
+
+print("\nBoa noite! Você está aprendendo sobre Allan Turing.\n")
+
+continuar = True
+while continuar == True:
+
+    opcao = int(
+        input(
+"""
+\nDigite o número correspondente ao menu que você deseja acessar:
+1 - Resumo
+2 - Doutorado
+3 - Contribuições
+4 - Principais Artigos
+5 - Citações
+6 - Sair\n
+"""
+        )
+    )
+
+    if opcao == 1:
+        print("1 - Resumo")
+        mensagem = resumo()
+
+    elif opcao == 2:
+        print("2 - Doutorado")
+        mensagem = doutorado()
+
+    elif opcao == 3:
+        print("3 - Contribuições")
+        mensagem = contribuicoes()
+
+    elif opcao == 4:
+        print("4 - Principais Artigos")
+        mensagem = artigos()
+
+    elif opcao == 5:
+        print("5 - Citações")
+        mensagem = citacoes()
+
+    elif opcao == 6:
+        mensagem = sair()
+        continuar = False
+
+    else:
+        mensagem = erro()
+
+    print(mensagem)
