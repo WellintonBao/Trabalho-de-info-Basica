cc855747cc97fd26d1939c9a01021f0624977858
sim0nato <simonato1905@gmail.com>
2023-02-15 21:18:41 -0300

    teste

--- lamport.py
+++ lamport.py
@@ -1,25 +1,25 @@
 def resumo():
-    mensagem = ""
+    mensagem = "Leslie Lamport é um cientista da computação estadunidense. Formou-se em matemática pelo Massachusetts Institute of Technology em 1960, com mestrado e doutorado em matemática pela Universidade Brandeis."
     return mensagem
 
 
 def doutorado():
-    mensagem = ""
+    mensagem = "O tema de sua tese de doutorado foi singularidades em equações diferenciais parciais analíticas..."
     return mensagem
 
 
 def contribuicoes():
-    mensagem = ""
+    mensagem = "Suas pesquisas contribuíram com a fundação da teoria de sistemas distribuídos. Além de introduzir novos conceitos na ciência computacional, tais como relógios lógicos (logical clocks) e a relação antes-depois, bem como as falhas Bizantinas."
     return mensagem
 
 
 def artigos():
-    mensagem = "Time, Clocks, and the Ordering of Events in a Distributed System","\nDistributed snapshots: determining global states of distributed systems","\nThe Byzantine Generals Problem."
+    mensagem = ""
     return mensagem
 
 
 def citacoes():
-    mensagem = "'A distributed system is one in which the failure of a computer you didn't even know existed can render your own computer unusable.'", "\n'I think in other things that I've done, I can look back and see: This idea developed from something else.'" , "\n'Thinking doesn't guarantee that we won't make mistakes. But not thinking guarantees that we will.'"
+    mensagem = ""
     return mensagem
 
 
